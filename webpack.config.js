d1f9a652a25cdea1ab533cd4313ab090c7dce622
EMH <morfinworking@gmail.com>
2022-03-22 14:33:54 -0600

    Add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index_bundle.js',
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+    ],
+  },
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      templateParameters: {
+        title: 'Digital BDay Invitation',
+        header: 'You were invited!',
+      },
+    }),
+  ],
+};
